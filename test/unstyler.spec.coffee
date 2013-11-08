df775274251562dfbf47007edf023fc42df581c4
Tim Dettrick <t.dettrick@uq.edu.au>
2013-11-08 16:03:28 +1000

    Stripping out jQuery dependency - we'll do this with regex.

--- test/unstyler.spec.coffee
+++ test/unstyler.spec.coffee
@@ -1,13 +1,18 @@
 # :indentSize=2:tabSize=2:noTabs=true:
 
-jsdom = require("jsdom")
+fs = require('fs')
 expect = require('chai').expect
 coffeescript = require("coffee-script")
-jqueryFactory = require('jquery')
 unstyle = require(__dirname+"/../src/unstyler.coffee").unstyle
-
 
 describe '.unstyle', () ->
   it 'should leave clean html alone', () ->
     html = '<ul><li>a</li><li>b</li></ul>'
-    expect(unstyle(html)).to.equal(html) 
+    expect(unstyle(html)).to.equal(html)
+  
+  it.skip 'should unmangle the test document', () ->
+    fs.readFile __dirname+'/fixture/word.html', { 
+      encoding: 'UTF-8'
+    }, (err, html) ->
+      throw err if (err)
+      expect(unstyle(html)).not.to.equal(html) 
--- test/unstyler.spec.js
+++ test/unstyler.spec.js
@@ -1,21 +1,29 @@
 (function() {
-  var coffeescript, expect, jqueryFactory, jsdom, unstyle;
+  var coffeescript, expect, fs, unstyle;
 
-  jsdom = require("jsdom");
+  fs = require('fs');
 
   expect = require('chai').expect;
 
   coffeescript = require("coffee-script");
 
-  jqueryFactory = require('jquery');
-
   unstyle = require(__dirname + "/../src/unstyler.coffee").unstyle;
 
   describe('.unstyle', function() {
-    return it('should leave clean html alone', function() {
+    it('should leave clean html alone', function() {
       var html;
       html = '<ul><li>a</li><li>b</li></ul>';
       return expect(unstyle(html)).to.equal(html);
+    });
+    return it.skip('should unmangle the test document', function() {
+      return fs.readFile(__dirname + '/fixture/word.html', {
+        encoding: 'UTF-8'
+      }, function(err, html) {
+        if (err) {
+          throw err;
+        }
+        return expect(unstyle(html)).not.to.equal(html);
+      });
     });
   });
 
